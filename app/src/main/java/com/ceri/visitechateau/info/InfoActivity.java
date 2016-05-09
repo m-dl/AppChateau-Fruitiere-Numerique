6bcf40b628a4ae705b03562b1dc488234084b46c
m-dl <fullcontactsoldier@gmail.com>
2016-05-09 16:16:59 +0200

    Ajout 3 boutons flottant, 1 visite info qui lance activité visite info 1 pour monter l'etage 1 pour descendre
    map et PI qui se change bien (statiquement pour le moment)

---
+++ app/src/main/java/com/ceri/visitechateau/info/InfoActivity.java
@@ -0,0 +1,40 @@
+package com.ceri.visitechateau.info;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.ceri.visitechateau.R;
+
+public class InfoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_info);
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+    }
+
+    @Override
+    protected void onPostCreate(Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
+    }
+}
--- app/src/main/java/com/ceri/visitechateau/main/MainActivity.java
+++ app/src/main/java/com/ceri/visitechateau/main/MainActivity.java
@@ -4,12 +4,17 @@ import android.Manifest;
 import android.app.Activity;
 import android.content.Context;
 import android.content.DialogInterface;
+import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.content.res.Resources;
 import android.os.Bundle;
 import android.os.Handler;
+import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.NavigationView;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.app.ActivityOptionsCompat;
 import android.support.v4.content.ContextCompat;
+import android.support.v4.util.Pair;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
@@ -17,12 +22,14 @@ import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.LinearLayout;
 import android.widget.Toast;
 
 import com.ceri.visitechateau.R;
 import com.ceri.visitechateau.entities.chateau.Visit;
 import com.ceri.visitechateau.files.FileManager;
+import com.ceri.visitechateau.info.InfoActivity;
 import com.ceri.visitechateau.params.AppParams;
 import com.ceri.visitechateau.tileview.TileViewTools;
 import com.ceri.visitechateau.tool.ScreenParam;
@@ -31,6 +38,7 @@ import com.qozix.tileview.TileView;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 import static com.ceri.visitechateau.tool.ConnectionManager.isNetworkAvailable;
 
@@ -53,6 +61,15 @@ public class MainActivity extends AppCompatActivity {
 
 	@Bind(R.id.toolbar)
 	Toolbar m_Toolbar;
+
+	@Bind(R.id.info)
+	FloatingActionButton m_FABInfo;
+
+	@Bind(R.id.map_floors_up)
+	FloatingActionButton m_FABFloorsUp;
+
+	@Bind(R.id.map_floors_down)
+	FloatingActionButton m_FABFloorsDown;
 
 	private TileView tileView;
 	private LinearLayout linearLayout;
@@ -112,7 +129,7 @@ public class MainActivity extends AppCompatActivity {
 		// size of original image at 100% scale
 		tileView.setSize(4000, 3561);
 		// detail levels
-		tileView.addDetailLevel(1.000f, "maps/%col%_%row%.jpg", "maps/planchateau.jpg");
+		tileView.addDetailLevel(1.000f, "maps/floor_1/%col%_%row%.jpg", "maps/floor_1/planchateau.jpg");
 		// let's use 0-1 positioning...
 		tileView.defineRelativeBounds(0, 0, 1, 1);
 		// center markers along both axes
@@ -125,8 +142,34 @@ public class MainActivity extends AppCompatActivity {
 		TileViewTools.frameTo(tileView, 0.5, 0.5);
 		// add some pins...
 		TileViewTools.addPin(tileView, getContext(), 0.5125,0.39666666666666667);
-		TileViewTools.addPin(tileView, getContext(), 0.1175,0.39666666666666667);
-		TileViewTools.addPin(tileView, getContext(), 0.895,0.13);
+		TileViewTools.addPin(tileView, getContext(), 0.1175, 0.39666666666666667);
+		TileViewTools.addPin(tileView, getContext(), 0.895, 0.13);
+	}
+
+	private void initMap2() {
+		linearLayout = (LinearLayout) findViewById(R.id.map);
+		// multiple references
+		tileView = new TileView(this);
+		LinearLayout.LayoutParams tileViewLayout = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, 0, 1);
+		linearLayout.addView(tileView, tileViewLayout);
+		// size of original image at 100% scale
+		tileView.setSize(4000, 2656);
+		// detail levels
+		tileView.addDetailLevel(1.000f, "maps/floor_2/%col%_%row%.jpg", "maps/floor_2/planchateau.jpg");
+		// let's use 0-1 positioning...
+		tileView.defineRelativeBounds(0, 0, 1, 1);
+		// center markers along both axes
+		tileView.setMarkerAnchorPoints(-0.5f, -0.5f);
+		// add marker event when touched
+		tileView.addMarkerEventListener(TileViewTools.markerEventListener);
+		// scale it down to manageable size
+		tileView.setScale(0.5);
+		// center the frame
+		TileViewTools.frameTo(tileView, 0.5, 0.5);
+		// add some pins...
+		TileViewTools.addPin(tileView, getContext(), 0.2125,0.39666666666666667);
+		TileViewTools.addPin(tileView, getContext(), 0.5175, 0.39666666666666667);
+		TileViewTools.addPin(tileView, getContext(), 0.695, 0.13);
 	}
 
 	private void selectLanguage() {
@@ -174,23 +217,23 @@ public class MainActivity extends AppCompatActivity {
 		m_DrawerLayout.setDrawerListener(m_DrawerToggle);
 		m_DrawerLayout.isDrawerOpen(m_NavigationView);
 		m_NavigationView.setNavigationItemSelectedListener(
-			new NavigationView.OnNavigationItemSelectedListener() {
-				MenuItem m_menuItem;
+				new NavigationView.OnNavigationItemSelectedListener() {
+					MenuItem m_menuItem;
 
-				@Override
-				public boolean onNavigationItemSelected(MenuItem menuItem) {
-					m_DrawerLayout.closeDrawers();
-					m_menuItem = menuItem;
-					new Handler().postDelayed(new Runnable() {
-						@Override
-						public void run() {
-							m_menuItem.setChecked(true);
-							navigationDrawerItemSelected(m_menuItem.getItemId(), m_menuItem.getTitle().toString());
-						}
-					}, 250);
-					return false;
-				}
-			});
+					@Override
+					public boolean onNavigationItemSelected(MenuItem menuItem) {
+						m_DrawerLayout.closeDrawers();
+						m_menuItem = menuItem;
+						new Handler().postDelayed(new Runnable() {
+							@Override
+							public void run() {
+								m_menuItem.setChecked(true);
+								navigationDrawerItemSelected(m_menuItem.getItemId(), m_menuItem.getTitle().toString());
+							}
+						}, 250);
+						return false;
+					}
+				});
 	}
 
 	public void navigationDrawerItemSelected(int position, String title) {
@@ -205,6 +248,37 @@ public class MainActivity extends AppCompatActivity {
 				takePicture.photo();
 			}
 		}
+	}
+
+	@OnClick(R.id.info)
+	public void onInfoClick() {
+		// launch visit info activity
+		ActivityOptionsCompat options = ActivityOptionsCompat.makeSceneTransitionAnimation(
+				m_Activity,
+				Pair.create((View) m_FABInfo, getResources().getString(R.string.ImageTransition))
+		);
+		launchVisitInfo(options);
+	}
+
+	@OnClick(R.id.map_floors_up)
+	public void onFloorUpClick() {
+		// remove all view or it doesn't work
+		linearLayout.removeAllViewsInLayout();
+		// rebuild the new map
+		initMap2();
+	}
+
+	@OnClick(R.id.map_floors_down)
+	public void onFloorDownClick() {
+		// remove all view or it doesn't work
+		linearLayout.removeAllViewsInLayout();
+		// rebuild the new map
+		initMap2();
+	}
+
+	private void launchVisitInfo(ActivityOptionsCompat options) {
+		Intent intent = new Intent(m_Activity, InfoActivity.class);
+		ActivityCompat.startActivity(m_Activity, intent, options.toBundle());
 	}
 
 	@Override
